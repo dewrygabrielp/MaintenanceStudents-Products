edc392c1ef0bf424923e53855a445d30be4f385c
dewrygabrielp <87697718+dewrygabrielp@users.noreply.github.com>
2021-07-23 16:48:38 +0800

    Add files via upload

---
+++ MantenimientoProducto/CapaNegocios/N_Mantenimiento.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaComun;
+using CapaDatos;
+
+
+
+namespace CapaNegocios
+{
+    public class N_Mantenimiento
+    {
+        //Los procedimientos en esta capa estaran relacionados con los procedimientos de la D_Mantenimientos para servir como puente con la capa presentacion
+
+        public static DataTable Mostrar()
+        {
+            
+            return new D_Mantenimiento().Mostrar();
+        }
+        public DataTable Buscar(E_Mantenimiento Categoria)
+        {
+            D_Mantenimiento ObjD = new D_Mantenimiento();
+            return ObjD.Buscar(Categoria);
+            
+        }
+        public string Insertar(E_Mantenimiento Categoria)
+        {
+            D_Mantenimiento ObjD = new D_Mantenimiento();
+            return ObjD.Insertar(Categoria);
+        }
+         public static string Editar(int idproducto, string codigo, string nombre, string descripcion)
+        {
+             D_Mantenimiento ObjD = new D_Mantenimiento();
+             E_Mantenimiento ObjE = new E_Mantenimiento();
+             ObjE.IDproducto = idproducto;
+             ObjE.Codigo = codigo;
+             ObjE.Nombre = nombre;
+             ObjE.Descripcion = descripcion;
+             return ObjD.Editar(ObjE);
+        }
+        public static string Eliminar(int idproducto)
+        {
+            D_Mantenimiento ObjD = new D_Mantenimiento();
+            E_Mantenimiento ObjE = new E_Mantenimiento();
+            ObjE.IDproducto = idproducto;
+
+            return ObjD.Eliminar(ObjE);
+        }
+    }
+}
---
+++ MantenimientoProducto/CapaNegocios/Nregistro.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace RegistroAlumno
+{
+    public class Nregistro
+    {
+        //Metodo para insertar desde la clase Dregistro
+
+        public static string Insertar(string codigo, string nombre, string apellido, string direccion)
+        {
+            Dregistro Obj = new Dregistro();
+            Obj.Codigo = codigo;
+            Obj.Nombre = nombre;
+            Obj.Apellido = apellido;
+            Obj.Direccion = direccion;
+
+            return Obj.Insertar(Obj);
+        }
+
+        //Metodo para Editar que llama al metodo Editar de la clase Dregistro
+
+        public static string Editar(int idalumno, string codigo, string nombre, string apellido, string direccion)
+        {
+            Dregistro Obj = new Dregistro();
+            Obj.IdAlumnos = idalumno;
+            Obj.Codigo = codigo;
+            Obj.Nombre = nombre;
+            Obj.Apellido = apellido;
+            Obj.Direccion = direccion;
+
+            return Obj.Editar(Obj);
+        }
+
+        //Metodo Eliminar que llama al metodo Eliminar de la clase Dregistro
+
+        public static string Eliminar(int idalumno)
+        {
+            Dregistro Obj = new Dregistro();
+            Obj.IdAlumnos = idalumno;
+            
+            return Obj.Eliminar(Obj);
+        }
+
+        //Metodo Buscar por nombre que llama al metodo BuscarNombre de la clase Dregistro
+
+        public static DataTable BuscarNombre(string textobuscar)
+        {
+            Dregistro Obj = new Dregistro();
+            Obj.Textobuscar = textobuscar;
+
+
+            return Obj.BuscarNombre(Obj);
+        }
+
+        //Metodo Mostrar que llama al metodo Mostrar de la clase Dregistro
+
+        public static DataTable Mostrar()
+        {
+            return new Dregistro().Mostrar();
+
+        }
+    }
+}
